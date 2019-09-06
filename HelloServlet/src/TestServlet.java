511c53cc153402efefb2ad176bf25d743c080e18
exyskns <freelancerelease@gmail.com>
2019-09-06 20:50:10 +0900

    0906

--- HelloServlet/src/TestServlet.java
+++ HelloServlet/src/TestServlet.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -6,39 +7,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class TestServlet
- */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
-    public TestServlet() {
-        super();
-    }
+public TestServlet() {
+super();
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	request.setCharacterEncoding("UTF-8");
-    	response.setContentType("text/html; charset=UTF-8");
+}
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+ServletException, IOException {
+request.setCharacterEncoding("UTF-8");
+response.setContentType("text/html; charset=UTF-8");
+String username=request.getParameter("username");
+String password=request.getParameter("password");
+System.out.println(username);
+System.out.println(password);
+PrintWriter out=response.getWriter();
 
-    	String username = request.getParameter("username");
-    	String password = request.getParameter("password");
-    	System.out.println(username);
-    	System.out.println(password);
+out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
+}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+ServletException, IOException {
+request.setCharacterEncoding("UTF-8");
+response.setContentType("text/html; charset=UTF-8");
+String username=request.getParameter("username");
+String password=request.getParameter("password");
+System.out.println(username);
+System.out.println(password);
+PrintWriter out=response.getWriter();
 
-    	PrintWriter out = response.getWriter();
-    	out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
-		}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
-
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		System.out.println(username);
-		System.out.println(password);
-
-		PrintWriter out = response.getWriter();
-		out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
-	}
+out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
+}
 }
--- HelloServlet/src/WelcomeServlet.java
+++ HelloServlet/src/WelcomeServlet.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,21 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/WelcomeServlet")
 public class WelcomeServlet extends HttpServlet {
-	public WelcomeServlet(){
-        super();
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
-		String username = request.getParameter("username");
-
-		System.out.println(username);
-
-		PrintWriter out = response.getWriter();
-		out.println("<html><head></head><body><br>"+username+"さん、ようこそ！</body></html>");
-	}
+public WelcomeServlet() {
+super();
+}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+ServletException, IOException {
+request.setCharacterEncoding("UTF-8");
+response.setContentType("text/html; charset=UTF-8");
+String username = request.getParameter("username");
+System.out.println(username);
+PrintWriter out=response.getWriter();
+out.println("<html><head></head><body><br>"+username+"さん、ようこそ!</body></html>");
+}
 }
